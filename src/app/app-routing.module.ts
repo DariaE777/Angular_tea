042d791fa81e5d8405dc1f12ec658483b101c445
Daria E <das333@yandex.ru>
2025-10-08 20:11:45 +0300

    AngularTea

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./components/pages/main/main.component";
+import {CatalogueComponent} from "./components/pages/catalogue/catalogue.component";
+import {OrderComponent} from "./components/pages/order/order.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'catalogue', component: CatalogueComponent},
+  {path: 'order', component: OrderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
